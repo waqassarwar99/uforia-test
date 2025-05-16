bf592089e4a192791ee553335086ecf8a2ab1a1a
waqassarwar1 <muhammad.waqas@innovotechnologies.com>
2025-05-16 19:16:10 +0500

    added the toast component

---
+++ src/ToastComponent.js
@@ -0,0 +1,58 @@
+// components/ToastContainer.jsx
+import React, { Fragment, useContext } from "react";
+import { ToastContext } from "./ToastContext";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { SnackbarContent } from "@mui/material";
+
+export default function ToastComponent() {
+  const { toasts, handleLikedToast, handleCloseToast } = useContext(ToastContext);
+  const action = (toast) => {
+    return (
+      <Fragment>
+        <Button
+          color="primary"
+          size="small"
+          onClick={() => handleLikedToast(toast)}
+        >
+          Like
+        </Button>
+        <Button
+          color="inherit"
+          size="small"
+          onClick={() => handleCloseToast(toast.id)}
+        >
+          X
+        </Button>
+      </Fragment>
+    );
+  };
+
+  return (
+    <Box>
+      {toasts?.map((toast, index) => (
+        <Box
+          key={toast.id}
+          sx={{
+            position: "fixed",
+            bottom: 20 + index * 55,
+            right: 20,
+            width: 400,
+            zIndex: 10,
+          }}
+        >
+          <SnackbarContent
+            message={
+              <Fragment>
+                {toast.data.firstName} {toast.data.lastName}
+                <br />
+                {toast.data.email}
+              </Fragment>
+            }
+            action={action(toast)}
+          />
+        </Box>
+      ))}
+    </Box>
+  );
+}
