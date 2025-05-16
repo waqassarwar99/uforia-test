b1cead1e1fe74c808a1d34331638cdd259ddb1ee
waqassarwar1 <muhammad.waqas@innovotechnologies.com>
2025-05-16 19:17:45 +0500

    updated the content file

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import React, { useContext } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { ToastContext } from "./ToastContext";
 
 export default function Content() {
+  const { likedToasts } = useContext(ToastContext);
   return (
-    <Box sx={{marginTop: 3}}>
+    <Box sx={{ marginTop: 3 }}>
       <Typography variant="h4">Liked Form Submissions</Typography>
 
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
+      <Typography variant="body1" sx={{ fontStyle: "italic", marginTop: 1 }}>
+        {likedToasts?.length === 0 ? (
+          <Typography
+            variant="body1"
+            sx={{ fontStyle: "italic", marginTop: 1 }}
+          >
+            No liked submissions yet.
+          </Typography>
+        ) : (
+          likedToasts?.map((toast) => (
+            <Box
+              key={toast.id}
+              sx={{ mt: 2, p: 2, border: "1px solid #ccc", borderRadius: 2 }}
+            >
+              <Typography>
+                {toast.data.firstName} {toast.data.lastName} (
+                {toast.data.email})
+              </Typography>
+            </Box>
+          ))
+        )}
       </Typography>
     </Box>
   );
